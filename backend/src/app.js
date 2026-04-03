008728da3dc91396780153cabf37ba2bb788584d
Ananyo mitra <ananyomitra310@gmail.com>
2026-04-03 11:46:07 +0530

    fouth commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,9 @@ app.use(express.json());
 app.use(cookieParser())
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173',
+        'https://moodify-ej0p.onrender.com'],
+
     credentials: true
 }))
 
--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/users/",
+  baseURL: "https://moodify-ej0p.onrender.com/api/users/",
   withCredentials: true,
 });
 
