4955552b556292bcb1188c127972b7cb6b661bdf
Ananyo mitra <ananyomitra310@gmail.com>
2026-04-03 13:36:19 +0530

    fifteeth commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,20 +30,5 @@ app.use('/api/songs/', songRouter);
 app.use(express.static("../public"))
 
 
-const path = require('path') ;
-const { fileURLToPath } = require("url");
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// 1. Serve static files from the frontend build folder
-// Adjust the path '../../frontend/dist' to match your folder structure
-app.use(express.static(path.join(__dirname, "../public")));
-
-// 2. THE CATCH-ALL ROUTE (Fixes the /login 404)
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public", "index.html"));
-});
-
 
 module.exports = app
